576660412043a724fb3ad7988a07ff81cc73e241
hlapov <alxlapov@yandex.ru>
2015-07-16 19:53:47 +0300

    requerement for api search xml structure test

---
+++ src/test/java/com/icanbuy/requirements/Api.java
@@ -0,0 +1,16 @@
+package com.icanbuy.requirements;
+
+/**
+ * Requirements for api requests wil be gathered here
+ */
+public class Api {
+    /**
+     * the most basic request, should return list of advertisers based on submitted values
+     */
+    public class Search {
+        /**
+         * xml in response should have correct structure
+         */
+        public class XMLStructure {}
+    }
+}
