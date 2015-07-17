328d0c8212480f0b91a08beb0aa2468e8b2e6171
hlapov <alxlapov@yandex.ru>
2015-07-17 15:04:23 +0300

    basic settings: urls

---
+++ src/main/java/com/icanbuy/utils/Settings.java
@@ -0,0 +1,16 @@
+package com.icanbuy.utils;
+
+/**
+ * General constants like urls, paths, e.t.c.
+ */
+public class Settings {
+    //urls
+    private static final String DEMO = "http://demo.icanbuy.com";
+    private static final String AWSUAT = "http://awsuat.icanbuy.com";
+
+    public static final String CURRENT_SERVER = DEMO;
+    /**
+     * same as {@link #CURRENT_SERVER} but with out http:// (https://)
+     */
+    public static final String CURRENT_SERVER_SHORT = CURRENT_SERVER.replaceAll("https?://", "");
+}
