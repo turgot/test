dec8f80473cb49ee96918a40260f5d411c56647e
hlapov <alxlapov@yandex.ru>
2015-07-17 15:00:25 +0300

    prepare structure for search xml verification test

---
+++ src/test/java/com/icanbuy/cases/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Pure api tests, ui not required: send request and check xml/json
+ */
+package com.icanbuy.cases.api;
---
+++ src/test/java/com/icanbuy/steps/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Steps for tests that not require ui, only api: send request and check server answer
+ */
+package com.icanbuy.steps.api;
